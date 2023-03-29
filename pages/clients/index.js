ccbef2589eb4e4d7e7304634cb45dd26edda7aa3
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-03-29 17:00:50 -0600

    feat: add link in map array

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,29 @@
+import Link from "next/link";
+
 const Clients = () => {
+
+    const clients = [{
+        id: 1,
+        name: 'John Doe'
+    } ,{
+        id: 2,
+        name: 'Diego Arturo'
+    }]
     return (<div>
         <h1>Clients Main Page</h1>
+        <ul>
+            {clients.map(client => (
+                <li
+                    key={client.id}
+                >
+                    <Link
+                        href={`/clients/${client.id}`}
+                    >
+                        {client.name}
+                    </Link>
+                </li>
+            ))}
+        </ul>
     </div>)
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Hi there</div>
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li><Link href="/portfolio">Portfolio</Link></li>
+        <li><Link href="/clients">Clients</Link></li>
+      </ul>
+    </div>
   )
 }
