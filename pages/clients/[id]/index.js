d4a5cd8379e7edd9e3552b545a3678067d203c22
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-03-29 17:30:53 -0600

    feat: add navigation event handler

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,8 +2,21 @@ import { useRouter } from "next/router";
 
 const ClientProjects = () => {
     const router = useRouter();
+
+    const loadProjectHandler = () => {
+        // router.replace(`/clients/${router.query.id}/1`);
+        router.push({
+            pathname: '/clients/[id]/[projectId]',
+            query: {
+                id: router.query.id,
+                projectId: 1,
+            }
+        })
+    }
+
     return (<div>
         <h1>Client {router.query.id} Projects</h1>
+        <button onClick={loadProjectHandler}>Load Project A</button>
     </div>)
 }
 
