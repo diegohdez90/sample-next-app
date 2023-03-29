a400b92650ef6b41b811c3a93de4c7688ccad205
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-03-29 13:51:33 -0600

    feat: add pages for portfolio

---
+++ pages/portfolio/[id].js
@@ -0,0 +1,7 @@
+const ProductId = () => {
+    return (<div>
+        <h1>Product Detail</h1>
+    </div>)
+}
+
+export default ProductId;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,7 @@
+const List = () => {
+    return (<div>
+        <h1>List Portfolio</h1>
+    </div>)
+}
+
+export default List;
