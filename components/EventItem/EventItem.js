a76d35ef19d25c6b56fd76f83668e5e3dcb00aca
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-03-30 10:22:04 -0600

    style: add css module to use classes

--- components/EventItem/EventItem.js
+++ components/EventItem/EventItem.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import classes from './EventItem.module.css';
+
 const EventItem = (props) => {
     const readableDate = new Date(props.date).toLocaleDateString('en-US', {
         day: 'numeric',
@@ -12,15 +14,15 @@ const EventItem = (props) => {
 
     const eventLink = `/events/${props.id}`
     return (<div>
-        <h4>Event Item</h4>
-        <div>
-            <h5>{props.title}</h5>
-            <Image src={props.image} alt="image-event-detail" width='100' height='100'
-             />
-            <p>{props.description}</p>
-            <p>{formattedAddress}</p>
-            <p>{readableDate}</p>
-            <Link href={eventLink}>Event Link</Link>
+        <div className={classes.card}>
+            <Image src={props.image} alt="image-event-detail" width='100' height='100' />
+            <div className={classes.container}>
+                <h5>{props.title}</h5>
+                <p>{props.description}</p>
+                <p>{formattedAddress}</p>
+                <p>{readableDate}</p>
+                <Link href={eventLink}>Event Link</Link>
+            </div>
         </div>
     </div>);
 }
