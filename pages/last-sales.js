37575f2ac333efebe37af27521ee7059f8abfa1e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-04-03 10:54:29 -0600

    feat: add swr to retrive api data

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,50 +1,75 @@
 import { Fragment, useEffect, useState } from "react";
+import useSwr from 'swr';
+import { fetcher } from "../utils/fetcher";
 
 const LastSales = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
+    const [sales, setSales] = useState([]);
+
+    const { data, error } = useSwr('https://nextjs-course-fc71e-default-rtdb.firebaseio.com/sales.json', fetcher);
+    
+
+    console.log('data', data);
+    console.log('error', error);
 
     useEffect(() => {
-        fetch('https://nextjs-course-fc71e-default-rtdb.firebaseio.com/sales.json')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                const sales = [];
-                console.log(data);
-                for (let key in data) {
-                    sales.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume
-                    });
-                }
-                setData(sales);
-                setIsLoading(false);
-            })
-            .catch(err => {
-                setIsLoading(false);
-            })
-    }, [])
+        console.log(data);
+        if (data) {
+            console.log(data);
+            const transformedSales = [];
+            for (let key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume
+                });
+            }
+            setSales(transformedSales);
+        }
+    }, [data])
 
-    if(isLoading) {
+    //useEffect(() => {
+    //    fetch('https://nextjs-course-fc71e-default-rtdb.firebaseio.com/sales.json')
+    //        .then(res => {
+    //            return res.json()
+    //        })
+    //        .then(data => {
+    //            const sales = [];
+    //            console.log(data);
+    //            for (let key in data) {
+    //                sales.push({
+    //                    id: key,
+    //                    username: data[key].username,
+    //                    volume: data[key].volume
+    //                });
+    //            }
+    //            setSales(sales);
+    //            setIsLoading(false);
+    //        })
+    //        .catch(err => {
+    //            setIsLoading(false);
+    //        })
+    //}, [])
+
+    if(!data || !sales) {
         return (<Fragment>
             <p>Loading...</p>
         </Fragment>);
     }
 
-    if(data.length === 0) {
+    if(error) {
         return (<Fragment>
             <p>No data found</p>
         </Fragment>)
     }
+
     return <Fragment>
         <div>
-            {data.length} Products available
+            {sales.length} Products available
             <ul>
                 {
-                    data.map(item => (
+                    sales.map(item => (
                         <li key={item.id}>{item.username}: {item.volume}</li>
                     ))
                 }
